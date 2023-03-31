fd4cd4e3eddf34215a0d91106df3033f21f1e376
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2023-03-31 10:17:25 -0500

    path

--- src/app/enviroments.ts
+++ src/app/enviroments.ts
@@ -1,4 +1,4 @@
 export class AppSettings{
-    public static API_PATH = "http://localhost:8080/api"
-    //public static API_PATH = "https://fenix-app-loca-sebas.herokuapp.com/api"
+    //public static API_PATH = "http://localhost:8080/api"
+    public static API_PATH = "https://fenix-app-loca-sebas.herokuapp.com/api"
 }
